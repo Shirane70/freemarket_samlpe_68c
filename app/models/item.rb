80a8428e825e13570fbd688e9081aa663202f062
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-03-11 12:26:58 +0900

    error

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,6 @@ class Item < ApplicationRecord
 
     belongs_to :seller, class_name: "User", optional: true
     belongs_to :buyer, class_name: "User", optional: true
-    belongs_to :category
+
 end
   
