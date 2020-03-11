3677aca0eeeb84bd914631993a4684bc719c69c9
Shirane70 <shirane.naoki.tree@ezweb.ne.jp>
2020-03-11 12:45:50 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_052447) do
+ActiveRecord::Schema.define(version: 2020_03_11_032806) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
